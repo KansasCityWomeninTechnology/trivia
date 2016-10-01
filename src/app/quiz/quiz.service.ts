a5f48e04b9606aa8de02b4f68abf069a6ae46dd2
Kayla Hennegin <kayla@bellafaithdesign.com>
2016-10-01 01:24:27 -0500

    use api

--- src/app/quiz/quiz.service.ts
+++ src/app/quiz/quiz.service.ts
@@ -8,8 +8,8 @@ export class QuizService {
   constructor(private http: Http) { }
 
   getQuestions() {
-    //return this.http.get('http://cocktail-trivia-api.herokuapp.com/api/sample')
-    return this.http.get('http://localhost:8080/api/sample')
+    //return this.http.get('http://localhost:8080/api/sample')
+    return this.http.get('http://cocktail-trivia-api.herokuapp.com/api/sample')
       .map((res: Response) => res.json());
   }
 
