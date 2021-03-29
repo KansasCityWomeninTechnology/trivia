faad69f531db5a63f6cb01373790b2cdd4703093
Alisa Duncan <7586237+alisaduncan@users.noreply.github.com>
2021-03-29 14:53:24 -0500

    completed code for data session

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { Route, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TriviaComponent } from './trivia/trivia.component';
+import { TriviaQuestionComponent } from './trivia-question/trivia-question.component';
 
 const routes: Route[] = [
   { path: 'trivia', component: TriviaComponent },
@@ -13,7 +14,8 @@ const routes: Route[] = [
 @NgModule({
   declarations: [
     AppComponent,
-    TriviaComponent
+    TriviaComponent,
+    TriviaQuestionComponent
   ],
   imports: [
     BrowserModule,
